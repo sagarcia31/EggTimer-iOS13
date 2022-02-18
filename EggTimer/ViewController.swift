1259246da8c61ddebca4e34d172991449590c16f
Marcelo Garcia <marcelo.sagarcia@ibm.com>
2022-02-18 10:43:02 -0300

    first commit

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,7 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: Variables
+    let eggTimes = ["Soft":3,"Medium":4,"Hard":5]
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+ 
+    // MARK: Outlets
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
     
-
+    // MARK: Actions for Buttons
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        
+        let hardness =  sender.currentTitle!
+        totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    // MARK: Private Methods
+    @objc func updateTimer(){
+        if(secondsPassed < totalTime) {
+            secondsPassed += 1
+            let percentageProgress = Float(secondsPassed) / Float(totalTime)
+            progressBar.progress = percentageProgress
+        } else {
+            timer.invalidate()
+            titleLabel.text = "Done"
+            
+            let alertController = UIAlertController.init(title: "Egg App", message: "O ovo tá pronto", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction.init(title: "Ok", style: .cancel, handler: nil))
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
 
 }
